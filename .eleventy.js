f2dffddfe040d60a04fbcc22f3a5f876cf8577c2
segyli <xiaoape@gmail.com>
2023-07-18 02:59:45 +0800

    added markdown syntax to first batch of content

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-const markdownIt = require('markdown-it'),
+const markdownIt = require("markdown-it"),
       eleventyNavPlugin = require("@11ty/eleventy-navigation"),
       { DateTime } = require("luxon");
 
@@ -9,17 +9,19 @@ module.exports = (eleventyConfig) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
-  eleventyConfig.addPassthroughCopy('dist');
+  eleventyConfig.addPassthroughCopy("dist");
+  // Copy `resource/` to `_site/resource`
+  eleventyConfig.addPassthroughCopy("resource");
 
   const options = {
     html: true,
     breaks: true,
     linkify: false
   };
-  eleventyConfig.setLibrary('md', markdownIt(options));
+  eleventyConfig.setLibrary("md", markdownIt(options));
 
   return {  
     // Use liquid in html templates
-    htmlTemplateEngine: 'liquid'
+    htmlTemplateEngine: "liquid"
   };
 };
