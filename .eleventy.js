ceb5739093c04a691079239aeb92127f843c872a
Sergey L <xiaoape@gmail.com>
2024-03-17 20:39:30 +0800

    feat: add rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,56 +1,75 @@
-const markdownIt = require("markdown-it");
-const eleventyNavPlugin = require("@11ty/eleventy-navigation");
-const { DateTime } = require("luxon");
-const env = require('dotenv').config();
-const inspect = require("util").inspect;
+const markdownIt = require("markdown-it")
+const eleventyNavPlugin = require("@11ty/eleventy-navigation")
+const { DateTime } = require("luxon")
+const env = require("dotenv").config()
+const inspect = require("util").inspect
+const xmlFiltersPlugin = require("eleventy-xml-plugin")
 
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addGlobalData("env", process.env);
+  eleventyConfig.addGlobalData("env", process.env)
 
-  eleventyConfig.addPlugin(eleventyNavPlugin);
-  eleventyConfig.addFilter("debug", (content) => `<pre>${inspect(content)}</pre>`);
+  eleventyConfig.addPlugin(eleventyNavPlugin)
+  eleventyConfig.addPlugin(xmlFiltersPlugin)
+
+  eleventyConfig.addFilter("debug", (content) => `<pre>${inspect(content)}</pre>`)
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toISODate();
-  });
+    return DateTime.fromJSDate(dateObj).toISODate()
+  })
 
   eleventyConfig.addFilter("cleanTag", (url) => {
-    let parts = url.split("/");
-    parts.shift();
-    parts.pop();
+    let parts = url.split("/")
+    parts.shift()
+    parts.pop()
 
-    return parts.join("/");
-  });
+    return parts.join("/")
+  })
 
   eleventyConfig.addFilter("jsonify", function(variable) {
-    return JSON.stringify(variable, null, 2);
-  });
+    return JSON.stringify(variable, null, 2)
+  })
 
   eleventyConfig.addFilter("getParentURL", (url) => {
-    let parts = url.split("/");
+    let parts = url.split("/")
 
-    const hashId = parts[parts.length - 2];
-    const secondToLastIndex = parts.length - 2;
-    parts.splice(secondToLastIndex, 1);
+    const hashId = parts[parts.length - 2]
+    const secondToLastIndex = parts.length - 2
+    parts.splice(secondToLastIndex, 1)
 
-    return parts.join("/") + "#" + hashId;
-  });
+    return parts.join("/") + "#" + hashId
+  })
+
+  eleventyConfig.addFilter("getNewestCollectionItemDate", (collections) => {
+    if(collections && collections.length > 0) {
+      const sortedCollections = collections.sort((a, b) => {
+        return b.date - a.date;
+      })
+
+      return sortedCollections[0].date
+    }
+
+    return null;
+  })
+
+  eleventyConfig.addFilter("dateToRfc3339", (date) => {
+    return DateTime.fromJSDate(date).toISO();
+  })
 
   // Copy `static/` to `_site/static`
-  eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy("static")
   // Copy root level static files
-  eleventyConfig.addPassthroughCopy("robots.txt");
-  eleventyConfig.addPassthroughCopy("404.html");
+  eleventyConfig.addPassthroughCopy("robots.txt")
+  eleventyConfig.addPassthroughCopy("404.html")
 
   const options = {
     html: true,
     breaks: true,
     linkify: false
-  };
-  eleventyConfig.setLibrary("md", markdownIt(options));
+  }
+  eleventyConfig.setLibrary("md", markdownIt(options))
 
   return {
     htmlTemplateEngine: "liquid",
     input: "source"
-  };
-};
+  }
+}
--- area/gtm/shopify-solving-cold-start-problem.md
+++ area/gtm/shopify-solving-cold-start-problem.md
@@ -9,5 +9,7 @@ date: 2024-03-17
 * 一个新业务需要联合上下游一起服务一个市场的，不能欠缺其中一些环节
 * 做一个新的业务，需要考虑上下游有哪些环节，有什么环节现在是缺少的，新业务对于上下游环节处于什么位置
 
+---
+
 Ref:
 - <a href="https://twitter.com/tobi/status/1494160021252358144?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1494160021252358144%7Ctwgr%5Ea6e1ad79e80188f73acab0fce500f2ad9c1733f1%7Ctwcon%5Es1_c10&ref_url=https%3A%2F%2Fstratechery.com%2F2022%2Fshopifys-evolution%2F" data-note-url="/read/essay-emergent-layers/emergent-layers-chapter-2-explosive-demand/">tobi lutke's tweet</a>
