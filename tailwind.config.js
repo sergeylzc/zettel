2e3a03b5cd3e1622ba20451119b54516e383f45b
Sergey L <xiaoape@gmail.com>
2024-03-18 18:46:24 +0800

    fix: back button css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./**/*.{html,css,js}"
+      "./**/*.{html,css,js,liquid}"
     ],
     theme: {
       extend: {
