7b6df8cc4ca0db157b932ba3818e72e8c365b6d0
Sergey L <xiaoape@gmail.com>
2024-03-22 15:38:10 +0800

    Update site content - 2024-03-22 15:38:10

--- script/generate-excerpts.js
+++ script/generate-excerpts.js
@@ -1,49 +1,46 @@
-const fs = require('fs');
-const path = require('path');
-const matter = require('gray-matter');
-const markdownIt = require('markdown-it');
-const md = new markdownIt();
+const fs = require('fs')
+const path = require('path')
+const matter = require('gray-matter')
+const markdownIt = require('markdown-it')
+const md = new markdownIt()
 
-const contentDir = './stream';
-const excerptLength = 60;
+const contentDirs = ['./stream', './reference']
+const excerptLength = 60
 
-// Utility function to process a single file
 function processFile(filePath) {
-
   if (path.basename(filePath) === 'index.md') {
     console.log('Skipping index.md')
-    return;
+    return
   }
 
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  const parsedContent = matter(fileContent);
+  const fileContent = fs.readFileSync(filePath, 'utf8')
+  const parsedContent = matter(fileContent)
 
-  if (!parsedContent.data.excerpt) { // Check if excerpt already exists
-    let excerpt = parsedContent.content.slice(0, excerptLength - 3) + '...';
-    excerpt = md.render(excerpt).replace(/<[^>]*>/g, '');
+  if (!parsedContent.data.excerpt) {
+    let excerpt = parsedContent.content.slice(0, excerptLength - 3) + '...'
+    excerpt = md.render(excerpt).replace(/<[^>]*>/g, '')
     
-    parsedContent.data.excerpt = excerpt;
-    const newContent = matter.stringify(parsedContent.content, parsedContent.data);
-    fs.writeFileSync(filePath, newContent);
-    console.log(`Excerpt added to ${filePath}`);
+    parsedContent.data.excerpt = excerpt
+    const newContent = matter.stringify(parsedContent.content, parsedContent.data)
+    fs.writeFileSync(filePath, newContent)
+    console.log(`Excerpt added to ${filePath}`)
   }
 }
 
-// Utility function to recursively process files in a directory
 function processDirectory(directory) {
-  const files = fs.readdirSync(directory);
+  const files = fs.readdirSync(directory)
 
   files.forEach(file => {
-    const filePath = path.join(directory, file);
-    const fileStat = fs.statSync(filePath);
+    const filePath = path.join(directory, file)
+    const fileStat = fs.statSync(filePath)
 
     if (fileStat.isDirectory()) {
       processDirectory(filePath); // Recursively process subdirectories
     } else if (filePath.endsWith('.md')) { 
-      processFile(filePath);
+      processFile(filePath)
     }
   });
 }
 
-// Start processing
-processDirectory(contentDir);
+// Start processing for each directory
+contentDirs.forEach(dir => processDirectory(dir))
